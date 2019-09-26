d1eb976efac102a112f0344ecd0a0d646dbaf47c
JallouliMohamed <medahmed.jallouli@gmail.com>
2019-09-26 03:01:35 -0200

    first release

---
+++ src/main/java/com/djamware/springangularauth/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.djamware.springangularauth.repositories;
+
+import com.djamware.springangularauth.models.Products;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface ProductRepository extends MongoRepository<Products, String> {
+
+    @Override
+    void delete(Products deleted);
+}
